d149c38700b594e5abf4b1c9a8d9f36fe191f68f
Dwina83 <dwinalarsson@gmail.com>
2023-10-11 11:49:03 +0200

    Update Program.cs
    Updating one more row

--- GitDemo/GitDemo/Program.cs
+++ GitDemo/GitDemo/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace GitDemo;
+﻿using System;
+
+namespace GitDemo;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hi Github world!");
+        Console.WriteLine("It's just demo tho!");
     }
 }
 
